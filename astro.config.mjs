27bfed32b8c2e9a8515f514443dd10a59726ccf5
leonel <leojesusbriones2@gmail.com>
2024-03-12 01:43:03 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: 'python-quiz'
+  // base: 'python-quiz'
 });
