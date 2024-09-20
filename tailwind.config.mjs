98fab66c4ae2fb2ce4e65cff48f78975c9f9abe7
Jesus Briones <leojesusbriones2@gmail.com>
2024-09-20 18:52:46 -0500

    Update color names

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,28 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'dark-navy': '#030712',
-				'light-blue': '#89DDFF',
-				'bright-pink': '#FF79BE',
-				yellow: '#FFDE58',
-				'midnight-blue': '#1E2541',
-			},
-			fontFamily: {
-				menlo: ['Menlo', 'Verdana', 'Geneva', 'Tahoma', 'sans-serif'],
-				JetBrainsMono: [
-					'JetBrainsMono',
-					'Verdana',
-					'Geneva',
-					'Tahoma',
-					'sans-serif',
-				],
-			},
-		},
-	},
-	plugins: [
-		require('tailwindcss-animated')
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'midnight-blue': '#030712',
+        'sky-blue': '#89DDFF',
+        'vivid-pink': '#FF79BE',
+      },
+      fontFamily: {
+        menlo: ['Menlo', 'Verdana', 'Geneva', 'Tahoma', 'sans-serif'],
+        JetBrainsMono: [
+          'JetBrainsMono',
+          'Verdana',
+          'Geneva',
+          'Tahoma',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animated')],
 }
